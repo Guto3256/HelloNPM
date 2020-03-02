fb75d3d0c8ec7416dd12f4383485359101171968
unknown <180070@bentoquirino.edu>
2020-03-02 15:40:02 -0300

    Adicionando nomes aleatórios

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const http = require('http');
+const { Form } = require('brazil-mocker');
+
+let nome = Form.names.fullName.generate();
 
 http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html; charset-utf-8'});
-    res.end('Hello NPM!');
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    res.end('Oi ' + nome);
 }).listen(3000);
 
 console.log('O node esta rodando na porta 3000, para encerrar o serviço pressione Ctrl + C! Este é o NPM!');
