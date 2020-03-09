6b9fa7e3968c3fc12a2e471199d91d854808af5c
unknown <180070@bentoquirino.edu>
2020-03-09 15:47:04 -0300

    Alterando nomes aleatórios para cada refresh

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const http = require('http');
 const { Form } = require('brazil-mocker');
 
-let nome = Form.names.fullName.generate();
+let nome;
 
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    nome = Form.names.fullName.generate();
     res.end('Oi ' + nome);
 }).listen(3000);
 
